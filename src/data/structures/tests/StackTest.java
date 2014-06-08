3b27a213ecad90669bfdca2b34bbc178750eaffc
otereshin <oleg.terjoshin@gmail.com>
2014-06-08 23:37:09 +0400

    fix tests

--- src/data/structures/tests/StackTest.java
+++ src/data/structures/tests/StackTest.java
@@ -6,57 +6,45 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Iterator;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import data.structures.stack.Stack;
 
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StackTest {
 
 	private Stack<String> stack;
 	private static final String TEST_STRING_NAME_HOMER = "HOMER";
-	private static final String TEST_STRING_NAME_ALEX = "AlEX";
+	private static final String TEST_STRING_NAME_ALEX = "ALEX";
 	private static final String TEST_STRING_NAME_DENIS = "DENIS";
 
-	@Test
-	public void CreateEmptyTest() {
+	@Before
+	public void dataPrepare() {
 		stack = new Stack<String>();
-		assertNotNull(stack);
+		stack.push(TEST_STRING_NAME_HOMER);
+		stack.push(TEST_STRING_NAME_ALEX);
 	}
 
 	@Test
-	public void AddSomeValuesToStackTest() {
-		stack.push(TEST_STRING_NAME_HOMER);
-		stack.push(TEST_STRING_NAME_ALEX);
+	public void addAndGetValueFromStackTest() {
 		stack.push(TEST_STRING_NAME_DENIS);
-		assertEquals(stack.size(), 2);
-	}
-	
-	@Test
-	public void GetValueFromStackTest() {
 		assertEquals(TEST_STRING_NAME_DENIS, stack.pop());
 	}
 
 	@Test
-	public void StackIterationTest() {
-		for (String name : stack) {
-			assertTrue(name.equals(TEST_STRING_NAME_ALEX)
-					|| name.equals(TEST_STRING_NAME_DENIS));
-		}
-	}
-
-	@Test
-	public void RemoveValueFromStackDuringIterationTest() {
+	public void stackIterationTest() {
 		Iterator<String> itr = stack.iterator();
 		while (itr.hasNext()) {
 			String name = itr.next();
-			assertEquals(TEST_STRING_NAME_ALEX, name);
-			itr.remove();
+			if (TEST_STRING_NAME_HOMER.equals(name)) {
+				itr.remove();
+			}
 		}
 		assertEquals(stack.size(), 1);
-	}
 
+	}
 }
